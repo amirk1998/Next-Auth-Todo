4ba259c48ea80761b30a360eb555e4532b8076c7
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-06-08 17:24:26 +0330

    Client Side Authentication

--- src/containers/Layout/Header.js
+++ src/containers/Layout/Header.js
@@ -2,12 +2,19 @@ import Link from 'next/link';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session, status } = useSession();
+  console.log(session);
+
   return (
     <nav className='flex w-full items-center justify-between rounded-xl bg-white px-8 py-6 shadow-lg'>
       <h1 className='w-full text-lg font-bold text-slate-800 lg:text-xl'>
         TodoList App
       </h1>
-      <ul className='flex w-full items-center justify-end gap-x-6 text-lg font-medium text-slate-800'>
+      <ul
+        className={`flex w-full items-center justify-end gap-x-6 text-base font-normal text-slate-800 lg:text-lg ${
+          status === 'loading' && !session ? 'opacity-0' : 'opacity-100'
+        }`}
+      >
         <li>
           <Link href='/'>
             <a>Home</a>
@@ -23,12 +30,17 @@ const Header = () => {
             <a>Profile</a>
           </Link>
         </li>
-        <li>
-          <button onClick={() => signIn('github')}>Sign In</button>
-        </li>
-        <li>
-          <button onClick={() => signOut()}>Sign Out</button>
-        </li>
+        {!session && status !== 'loading' && (
+          <li>
+            <button onClick={() => signIn('github')}>Sign In</button>
+          </li>
+        )}
+        {/*  */}
+        {session && (
+          <li>
+            <button onClick={() => signOut()}>Sign Out</button>
+          </li>
+        )}
       </ul>
     </nav>
   );
