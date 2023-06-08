85dfc04bc51e3a75df76e071cf583c7fb70a3c60
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-06-08 17:45:26 +0330

    Securing Pages from Client-Side

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,28 @@
 import Layout from '@/containers/Layout';
+import { signIn, useSession } from 'next-auth/react';
 
 const Profile = () => {
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn();
+    },
+  });
+
+  if (status === 'loading') {
+    return (
+      <Layout>
+        <div className='text-center text-xl font-semibold text-slate-800'>
+          Loading ...
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <h2 className=' w-full text-center text-lg font-semibold lg:text-2xl'>
-        Profile Page
+      <h2 className=' my-6 w-full text-center text-xl font-semibold lg:text-2xl'>
+        Hi {session.user.name}. Welcome to Profile Page
       </h2>
     </Layout>
   );
